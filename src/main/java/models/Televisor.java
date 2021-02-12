5fda509dcfac5fe1ae7fc8fece0825bebd44e3b7
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-12 08:37:06 -0500

    Documentacion clase Televisor agregada

--- src/main/java/models/Televisor.java
+++ src/main/java/models/Televisor.java
@@ -8,34 +8,65 @@ public class Televisor extends Electrodomestico{
     protected int pulgadas;
     protected boolean sintonizadorTDT;
 
+    /**
+     * Constructor clase Televisor
+     * @param consumo - Tipo Consumo
+     * @param procedencia - Tipo Procedencia
+     * @param pulgadas - Tipo int
+     * @param sintonizadorTDT - Tipo boolean
+     */
     public Televisor(Consumo consumo, Procedencia procedencia, int pulgadas, boolean sintonizadorTDT){
         super(consumo, procedencia);
         this.pulgadas = pulgadas;
         this.sintonizadorTDT = sintonizadorTDT;
     }
 
+    /**
+     * Constructor por defecto clase Television
+     * Inicializa todo a null, 0 o false como corresponda
+     */
     public Televisor(){
         super();
         this.pulgadas = 0;
         this.sintonizadorTDT = false;
     }
 
+    /**
+     * Getter tamaño del tv en pulgadas
+     * @return pulgadas - Tipo int
+     */
     public int darPulgadas(){
         return this.pulgadas;
     }
 
+    /**
+     * Getter dar Sintonizador
+     * @return sintonizadorTDT - Tipo boolean
+     */
     public boolean darSintonizador(){
         return this.sintonizadorTDT;
     }
 
+    /**
+     * Setter tamaño del Televisor
+     * @param pulgadas - Tipo int
+     */
     public void asignarPulgadas(int pulgadas){
         this.pulgadas = pulgadas;
     }
 
+    /**
+     * Setter sintonizadorTDT
+     * @param sintonizadorTDT - Tipo boolean
+     */
     public void asignarSintetizador(boolean sintonizadorTDT){
-        this.pulgadas = pulgadas;
+        this.sintonizadorTDT = sintonizadorTDT;
     }
 
+    /**
+     * Calcular el costo adicional del tv segun la propiedad sintonizadorTDT
+     * @return costo adicional - Tipo float
+     */
     public float calcularPrecioSintatizador(){
 
         float precio = 0;
@@ -45,11 +76,20 @@ public class Televisor extends Electrodomestico{
         return precio;
     }
 
+    /**
+     * Asignar precio
+     * Implementacion propia del metodo asignar precio de la clase electrodomestic
+     */
     @Override
     public void asignarPrecio(){
         this.precio = calcularPrecio();
     }
 
+    /**
+     * Calcular el precio del Televisor
+     * darPulgadas()>40?(precioBase + precioSintonizador) * 1.3: (precioBase + precioSintonizador);
+     * @return precioTelevisor - Tipo int
+     */
     private float calcularPrecio(){
         float precioTentativo = calcularPrecioConsumo() + calcularPrecioProcedencia() + calcularPrecioSintatizador();
         if(darPulgadas() > 40){
