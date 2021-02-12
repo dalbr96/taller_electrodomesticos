a646c258da1344890b342d7ea926ce6a4a332bc3
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-12 08:25:45 -0500

    Documentacion clase Nevera agregada

--- src/main/java/models/Nevera.java
+++ src/main/java/models/Nevera.java
@@ -6,24 +6,47 @@ import constants.Procedencia;
 public class Nevera extends Electrodomestico{
     protected int litros;
 
+    /**
+     * Constructor clase nevera
+     * @param consumo - tipo Consumo
+     * @param procedencia - tipo Procedencia
+     * @param litros - tipo int
+     */
     public Nevera(Consumo consumo, Procedencia procedencia, int litros){
         super(consumo, procedencia);
         this.litros = litros;
     }
 
+
+    /**
+     * Constructor por defecto de Nevera
+     * Inicializa las variables a null y cero
+     */
     public Nevera(){
         super();
         this.litros = 0;
     }
 
+    /**
+     * Accesor dar litros
+     * @return this.litros
+     */
     public int darLitros(){
         return this.litros;
     }
 
+    /**
+     * Setter de litros
+     * @param litros - tipo int
+     */
     public void asignarLitros(int litros){
         this.litros = litros;
     }
 
+    /**
+     * Calcula el incremento de precio dependiendo de la capacidad en litros de la nevera
+     * @return incremento en fraccion - tipo float
+     */
     public float calcularIncremento(){
 
         if(darLitros()>120){
@@ -38,6 +61,10 @@ public class Nevera extends Electrodomestico{
         }
     }
 
+    /**
+     * Implementacion de nevera del metodo asignar precio.
+     * Precio nevera = precioBase * incrementoEnFraccion
+     */
     @Override
     public void asignarPrecio(){
         this.precio = calcularIncremento() * (calcularPrecioConsumo() + calcularPrecioProcedencia());
