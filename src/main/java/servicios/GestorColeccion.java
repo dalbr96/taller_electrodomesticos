c081939213e9e56246bef3d9a0925c03f31a418b
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-12 08:45:03 -0500

    Documentacion clase GestorColeccion agregada

--- src/main/java/servicios/GestorColeccion.java
+++ src/main/java/servicios/GestorColeccion.java
@@ -6,6 +6,9 @@ import models.Electrodomestico;
 import models.Nevera;
 import models.Televisor;
 
+/**
+ * Clase gestora de la coleccion de electrodomesticos
+ */
 public class GestorColeccion {
 
     protected ArrayList<Electrodomestico> listaElectrodomesticos;
@@ -13,6 +16,11 @@ public class GestorColeccion {
     float precioNeveras;
     float precioElectrodomesticos;
 
+    /**
+     * Constructor clase GestorColeccion
+     * Inicializa todos los valores a su valor predeterminado menos listaElectrodomesticos
+     * listaElectrodomesticos = new ArrayList<>()
+     */
     public GestorColeccion(){
         listaElectrodomesticos = new ArrayList<>();
         precioNeveras = 0f;
@@ -20,27 +28,50 @@ public class GestorColeccion {
         precioTelevisores = 0f;
     }
 
-
+    /**
+     * Getter listaElectrodomesticos
+     * @return listaElectrodomesticos - Tipo ArrayList<Electrodomestico>
+     */
     public ArrayList<Electrodomestico> darListaElectrodomesticos(){
         return this.listaElectrodomesticos;
     }
 
+    /**
+     * Getter precio total televisores
+     * @return precioTelevisores - Tipo float
+     */
     public float darPrecioTelevisores(){
         return this.precioTelevisores;
     }
 
+    /**
+     * Getter precio total neveras
+     * @return precioNeveras - Tipo float
+     */
     public float darPrecioNeveras(){
         return this.precioNeveras;
     }
 
+    /**
+     * Getter precio total electrodomesticos
+     * @return precioElectrodomesticos - Tipo float
+     */
     public float darPrecioElectrodomesticos(){
         return this.precioElectrodomesticos;
     }
 
+    /**
+     * Agrega un nuevo electrodomestico a la propiedad listaElectrodomesticos
+     * @param electrodomestico - Tipo Electrodomestico
+     */
     public void agregarElectrodomestico(Electrodomestico electrodomestico){
         this.listaElectrodomesticos.add(electrodomestico);
     }
 
+    /**
+     * Calcula el total del precio de los televisores en la coleccion listaElectrodomesticos
+     * Asigna este valor a this.precioTelevisores
+     */
     public void calcularPrecioTelevisores(){
         for(Electrodomestico electrodomestico : darListaElectrodomesticos()){
             if(electrodomestico instanceof Televisor){
@@ -50,6 +81,10 @@ public class GestorColeccion {
         }
     }
 
+    /**
+     * Calcula el total del precio de los Electrodomesticos en la coleccion listaElectrodomesticos
+     * Asigna este valor a this.precioElectrodomesticos
+     */
     public void calcularPrecioElectrodomestico(){
         for(Electrodomestico electrodomestico: darListaElectrodomesticos()){
             if(!(electrodomestico instanceof Televisor) && !(electrodomestico instanceof Nevera)) {
@@ -59,6 +94,10 @@ public class GestorColeccion {
         }
     }
 
+    /**
+     * Calcula el total del precio de las Neveras en la coleccion listaElectrodomesticos
+     * Asigna este valor a this.precioNeveras
+     */
     public void calcularPrecioNeveras(){
         for(Electrodomestico electrodomestico:darListaElectrodomesticos()){
             if(electrodomestico instanceof Nevera){
@@ -68,6 +107,9 @@ public class GestorColeccion {
         }
     }
 
+    /**
+     * Invoca los metodos calcularPrecioTelevisores(), calcularPrecioElectrodomesticos() y calcularPrecioNeveras
+     */
     public void calcularTotales() {
         calcularPrecioTelevisores();
         calcularPrecioElectrodomestico();
